672df30755b8e8cd55d5eda715d7b97a6acf144c
Tushar <tuharbansal3366@gmail.com>
2024-11-06 22:22:41 +0530

    Initial commit with Node.js setup and .gitignore

---
+++ index.js
@@ -0,0 +1,24 @@
+let express=require("express");
+
+require("dotenv").config();
+
+let cookieparser=require("cookie-parser")
+let app=express();
+
+let cors=require("cors");
+let {PORT_NO}=process.env;
+// connect("mongodb://127.0.0.1:27017/user");
+
+
+app.use(cors());
+app.use(express.json()); 
+app.use(cookieparser())
+// ------------main routes---------------
+app.get("/", function(req, res){
+
+console.log("new express.request");
+res.json({"message":"hello from server"})
+})
+
+
+app.listen(PORT_NO,()=>console.log("express run at port no :"+PORT_NO));
