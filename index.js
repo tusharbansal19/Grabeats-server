b42b37f788288d7a6c291f60e718ce807fe73c9a
Tushar <tuharbansal3366@gmail.com>
2025-06-29 16:12:17 +0530

    my project

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // CORS middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', '*'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://grabeats.onrender.com', "*"],
   credentials: true
 }));
 
